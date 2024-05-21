a532908e88da768b9bb5a26363215223f9113b9b
HabibFahim15 <habibfahim71@gmail.com>
2024-05-21 22:40:43 +0600

    jwt related work proccecing.and verified the token

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
+app.use(express.json());
+//after install cookies then set for middleware.but first imported the cookie
+app.use(cookieParser());
 
 console.log(process.env.DB_PASS);
 
@@ -24,6 +31,31 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = async(req, res, next) => {
+  console.log('called', req.host, req.originalUrl)
+  next()
+}
+const verifyToken = async(req, res, next) =>{
+  const token = req.cookies?.token;
+  console.log('value of token:',token);
+  if(!token){
+    return res.status(401).send({message: 'not authorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error, decoded) =>{
+    //error
+    if(error){
+      console.log(error);
+      return res.status(401).send({message: 'unauthorized'})
+    }
+    // if token is valid then it would be decoded
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next()
+  })
+  
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,6 +64,25 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services')
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
+    //auth related api
+    // sending the cookie to client first.
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+
+      res
+      .cookie('token',token, {
+        httpOnly: true,
+        secure:false,
+      })
+      .send({success: true})
+    })
+
+
+    //services related api
     app.get('/services', async(req,res)=>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -42,7 +93,7 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
 
-      //specific value from array object if i want..otherwise fuul object will return.
+      //specific value from array object if i want..otherwise full object will return.
       const options ={
         projection: {title: 1, img:1, price: 1, service_id: 1},
       };
@@ -54,8 +105,10 @@ async function run() {
 
     //Bookings
 
-    app.get('/bookings', async(req,res)=>{
+    app.get('/bookings',verifyToken, async(req,res)=>{
       console.log(req.query.email);
+      // after send the cookey from booking.jsx checking what server got.
+      // console.log('tok tok token', req.cookies.token);
       let query ={};
       if (req.query?.email) {
         query = {email: req.query.email}
